4756d3aea03ebda9e6bb973be0af180ab58681b3
Wanderson S Lisboa <wandersonslisboa@gmail.com>
2024-03-24 01:47:19 -0300

    Projeto Codg6

---
+++ src/main/java/br/com/codg6/Codg6Application.java
@@ -0,0 +1,15 @@
+package br.com.codg6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codg6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codg6Application.class, args);
+
+		System.out.println("Ola mundo");
+	}
+
+}
---
+++ src/test/java/br/com/codg6/Codg6ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codg6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Codg6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
